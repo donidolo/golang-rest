ff9c1655fc9960e682c18e02577ef081fc968d15
Sutanto <sutanto.1010@gmail.com>
2021-07-17 12:19:35 +0700

    Added endpoint to get note by ID

--- Week-05/my-keep-backend/go.mod
+++ Week-05/my-keep-backend/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gin-gonic/gin v1.7.2
 	github.com/google/uuid v1.2.0
 	github.com/lib/pq v1.10.2 // indirect
+	xorm.io/builder v0.3.8 // indirect
 	xorm.io/xorm v1.1.2
 )
--- Week-05/my-keep-backend/main.go
+++ Week-05/my-keep-backend/main.go
@@ -3,17 +3,49 @@ package main
 import (
 	"time"
 
+	"my-keep-backend/db"
 	"my-keep-backend/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
-	"xorm.io/xorm"
+	"xorm.io/builder"
 )
 
+/*
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	//temp := v.X*v.X + v.Y*v.Y
+
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+	//(30 * 30) + (40 * 40)
+}
+
+func (v *Vertex) Scale(f float64) {
+	fmt.Println("X=", v.X)
+	fmt.Println("Y=", v.X)
+	v.X = v.X * f // X= 3 * 10
+	v.Y = v.Y * f // Y = 4 * 10
+}
+
 func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.X)
+	fmt.Println(v.Y)
+	v.Scale(10)
+	fmt.Println(v.Abs())
+}
+*/
+var dbClient db.Db
+
+func main() {
+	dbClient = db.Db{}
+	dbClient.Connect("postgres", "P@ssw0rd", "localhost", 5432, "keep", "postgres")
 	route := gin.Default()
-	route.Any("/ping", func(c *gin.Context) {
+	route.GET("/ping", func(c *gin.Context) {
 		username := "tanto"
 		print(username)
 		c.JSON(200, gin.H{
@@ -24,11 +56,11 @@ func main() {
 
 	route.POST("/note", func(c *gin.Context) {
 		var note models.Note
+		var err error
 		newId, _ := uuid.NewUUID()
 		note.Id = newId.String()
 		c.Bind(&note)
-		engine, err := xorm.NewEngine("postgres", "host=localhost port=5432 user=postgres password=P@ssw0rd dbname=keep sslmode=disable")
-		_, err = engine.Insert(&note)
+		_, err = dbClient.Conn.Insert(&note)
 		if err != nil {
 			c.JSON(503, gin.H{
 				"error": err.Error(),
@@ -36,6 +68,19 @@ func main() {
 		} else {
 			c.JSON(200, note)
 		}
+	})
+	route.GET("/note/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var note models.Note
+		_, err := dbClient.Conn.Where(builder.Eq{"id": id}).Get(&note)
+		if err != nil {
+			c.JSON(503, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			c.JSON(200, note)
+		}
+
 	})
 
 	route.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
